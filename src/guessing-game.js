30f2e57f1363f78aefd9fd92743eab0d6b587006
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-07-15 09:26:09 +0300

    make constructor

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor(options) {
+        this.number = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.round((this.min + this.max) / 2);
+        console.log(this.number);
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
